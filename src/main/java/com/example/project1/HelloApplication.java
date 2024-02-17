ad0019e9fb791d1fdfeaf9b91438cf25b92c9e76
fatimaSheaib <155558536+fatimaSheaib@users.noreply.github.com>
2024-02-17 22:40:03 +0200

    change images

--- src/main/java/com/example/project1/HelloApplication.java
+++ src/main/java/com/example/project1/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("sign-up.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Login Form!");
+        stage.setTitle("Sign Form!");
         stage.setScene(scene);
         stage.show();
     }
