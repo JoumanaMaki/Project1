c21d8f67898f4b92cd7914ce6fa931ec4966e44c
fatimaSheaib <155558536+fatimaSheaib@users.noreply.github.com>
2024-02-18 00:22:18 +0200

    change images

--- src/main/java/com/example/project1/GuestController.java
+++ src/main/java/com/example/project1/GuestController.java
@@ -1,7 +1,17 @@
+package com.example.project1;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class GuestController {
 
@@ -13,10 +23,26 @@ public class GuestController {
 
     @FXML
     private Button Teacherlogout;
-
     @FXML
     void switchForm(ActionEvent event) {
 
     }
+
+@FXML
+    void sign(ActionEvent event) {
+        try {
+            ((Stage)(((javafx.scene.Node) event.getSource()).getScene().getWindow())).close();
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("sign-up.fxml"));
+            AnchorPane anchorPane = fxmlLoader.load();
+            Stage stage = new Stage();
+            Scene scene=new Scene(anchorPane);
+            stage.setScene(scene);
+            stage.setTitle("Sign Up Page");
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 }
