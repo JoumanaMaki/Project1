e81d68ea7ab987da76d4a0a78b63165c53556f20
fatimaSheaib <155558536+fatimaSheaib@users.noreply.github.com>
2024-02-17 23:29:59 +0200

    change images

---
+++ src/main/java/com/example/project1/GuestController.java
@@ -0,0 +1,22 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class GuestController {
+
+    @FXML
+    private Button Teacherdashboard_btn;
+
+    @FXML
+    private Label Teachergreet_username;
+
+    @FXML
+    private Button Teacherlogout;
+
+    @FXML
+    void switchForm(ActionEvent event) {
+
+    }
+
+}
