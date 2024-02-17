cf0237591a76c6ad6e7ad5f4a2d1d97ad5c0668b
fatimaSheaib <155558536+fatimaSheaib@users.noreply.github.com>
2024-02-17 18:43:36 +0200

    changes to files

--- src/main/java/com/example/project1/TeacherPageController1.java
+++ src/main/java/com/example/project1/TeacherPageController1.java
@@ -5,16 +5,10 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-<<<<<<< HEAD
 import javafx.scene.Scene;
 import javafx.scene.chart.AreaChart;
 import javafx.scene.chart.LineChart;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
@@ -28,7 +22,7 @@ import java.io.IOException;
 import java.sql.Date;
 import java.time.LocalDate;
 
-public class TeacherPageController {
+public class TeacherPageController1 {
 
     @FXML
     private AnchorPane CollectMoneyForm;
@@ -414,7 +408,7 @@ public void addStudentDisplayData() {
                 String Course = parts[1].trim();
                 String dep = parts[2].trim();
                 double price = Double.parseDouble(parts[3].trim());
-                java.sql.Date date = java.sql.Date.valueOf(parts[4].trim());
+                Date date = Date.valueOf(parts[4].trim());
                 String status = parts[5].trim();
                 CourseData cData = new CourseData(courseid, Course, dep, price, date, status);
                 listData.add(cData);
@@ -435,37 +429,3 @@ public void addStudentDisplayData() {
         addCourse_status.getSelectionModel().select(0);
     }
 }
-=======
-import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.FileChooser;
-import javafx.stage.FileChooser.ExtensionFilter;
-import javafx.stage.Stage;
-
-import java.io.*;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.Period;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.ResourceBundle;
-
-public class TeacherPageController {
-
-}
->>>>>>> origin/master
